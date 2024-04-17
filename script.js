478f6e4c7ca2c514f59f1b44f22f848ba0b07b44
nam-web <65065658+nam-web@users.noreply.github.com>
2024-04-17 10:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function fibonacci(num) {
 // your code here
-  let a = 0, b = 1;
-
+  let a = 0, b = 1, sum = 1;
+	if(num <=1 )
+		return a;
   for (let i = 2; i <= num; i++) {
     let temp = a + b;
-    a = b;
-    b = temp;
+	  sum += temp;
+	  a = b;
+	  b = temp;
   }
 
-  return a;
+  return sum;
 }
 const num = prompt("Enter the value: ");
 const result = fibonacci(num); 
